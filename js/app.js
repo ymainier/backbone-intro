461c5d72dda5a645c06424fd065ec4c543efacfa
Yann Mainier <yann.mainier@gmail.com>
2014-05-25 00:03:52 +0200

    Ajout de pois (Collection, Model, Templates et Vues)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+var PoiModel = Backbone.Model.extend({});
+var PoiCollection = Backbone.Collection.extend({
+  model: PoiModel
+});
+
 var HomeView = Backbone.View.extend({
   render: function() {
     // 1. Récupérer le contenu du template
@@ -20,10 +25,41 @@ var AboutView = Backbone.View.extend({
   }
 });
 
+var PoiCollectionView = Backbone.View.extend({
+  render: function() {
+    // 1. Récupérer le contenu du template
+    var template = $('#template-pois-index').html();
+    // 2. Le compiler en html
+    var html = Handlebars.compile(template);
+    // 3. L'insérer dans le DOM
+    this.$el.html(html({models: this.collection.toJSON()}));
+  }
+});
+
+var PoiModelView = Backbone.View.extend({
+  render: function() {
+    // 1. Récupérer le contenu du template
+    var template = $('#template-pois-show').html();
+    // 2. Le compiler en html
+    var html = Handlebars.compile(template);
+    // 3. L'insérer dans le DOM
+    this.$el.html(html(this.model.toJSON()));
+  }
+});
+
+var poiCollection = new PoiCollection([
+  {id: 1, name: "Tour Eiffel", cotation: 3},
+  {id: 2, name: "Arc de Triomphe", cotation: 3},
+  {id: 3, name: "Musée national de Céramique", cotation: 2},
+  {id: 4, name: "Musée français de la Carte à Jouer", cotation: 1}
+]);
+
 var Router = Backbone.Router.extend({
   routes: {
     "": "home",
-    "about": "about"
+    "about": "about",
+    "pois": "poisIndex",
+    "pois/:id": "poisShow"
   },
 
   home: function() {
@@ -39,6 +75,34 @@ var Router = Backbone.Router.extend({
     $('#container').html(aboutView.el)
     console.log("about route");
   },
+
+  poisIndex: function() {
+    var poiCollectionView = new PoiCollectionView({
+      collection: poiCollection
+    });
+    poiCollectionView.render();
+    $('#container').html(poiCollectionView.el)
+    console.log("poisIndex route");
+  },
+  poisShow: function(id) {
+    var model = poiCollection.find(function (poi) {
+      return poi.id === +id;
+    });
+    var poiModelView = new PoiModelView({
+      model: model
+    });
+    poiModelView.render();
+    $('#container').html(poiModelView.el)
+    console.log("poisShow route " + id);
+  },
+});
+
+Handlebars.registerHelper('stars', function(count) {
+  var stars = "", i;
+  for (i = 0; i < count; i++) {
+    stars += "&#9733;";
+  }
+  return new Handlebars.SafeString(stars);
 });
 
 $(function() {
