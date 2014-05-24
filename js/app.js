23ca57a40e446956deaa1991388234a96f282fcd
Yann Mainier <yann.mainier@gmail.com>
2014-05-24 23:02:15 +0200

    Ajout d'une vue about et d'un router

--- js/app.js
+++ js/app.js
@@ -9,9 +9,39 @@ var HomeView = Backbone.View.extend({
   }
 });
 
+var AboutView = Backbone.View.extend({
+  render: function() {
+    // 1. Récupérer le contenu du template
+    var template = $('#template-about').html();
+    // 2. Le compiler en html
+    var html = Handlebars.compile(template);
+    // 3. L'insérer dans le DOM
+    this.$el.html(html);
+  }
+});
+
+var Router = Backbone.Router.extend({
+  routes: {
+    "": "home",
+    "about": "about"
+  },
+
+  home: function() {
+    var homeView = new HomeView();
+    homeView.render();
+    $('#container').html(homeView.el)
+    console.log("home route");
+  },
+
+  about: function() {
+    var aboutView = new AboutView();
+    aboutView.render();
+    $('#container').html(aboutView.el)
+    console.log("about route");
+  },
+});
 
 $(function() {
-  var homeView = new HomeView();
-  homeView.render();
-  $('#container').html(homeView.el)
+  new Router();
+  Backbone.history.start();
 });
