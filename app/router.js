5c0e53220995dcf33741b5bc0b84e86a5e53c2c6
Yann Mainier <yann.mainier@gmail.com>
2015-07-11 14:55:52 +0200

    Refactorisation du router

--- app/router.js
+++ app/router.js
@@ -7,52 +7,48 @@ var PoiCollectionView = require("./views/pois/index");
 var PoiModelView = require("./views/pois/show");
 
 var poiCollection = new PoiCollection([
-  {id: 1, name: "Tour Eiffel", cotation: 3},
-  {id: 2, name: "Arc de Triomphe", cotation: 3},
-  {id: 3, name: "Musée national de Céramique", cotation: 2},
-  {id: 4, name: "Musée français de la Carte à Jouer", cotation: 1}
+	{id: 1, name: "Tour Eiffel", cotation: 3},
+	{id: 2, name: "Arc de Triomphe", cotation: 3},
+	{id: 3, name: "Musée national de Céramique", cotation: 2},
+	{id: 4, name: "Musée français de la Carte à Jouer", cotation: 1}
 ]);
 
 module.exports = Backbone.Router.extend({
-  routes: {
-    "": "home",
-    "about": "about",
-    "pois": "poisIndex",
-    "pois/:id": "poisShow"
-  },
+	routes: {
+		"": "home",
+		"about": "about",
+		"pois": "poisIndex",
+		"pois/:id": "poisShow"
+	},
 
-  home: function() {
-    var homeView = new HomeView();
-    homeView.render();
-    $('#container').html(homeView.el);
-    console.log("home route");
-  },
+	mainContainer : "#container",
 
-  about: function() {
-    var aboutView = new AboutView();
-    aboutView.render();
-    $('#container').html(aboutView.el);
-    console.log("about route");
-  },
+	renderBaseRoute : function (ViewClass, configuration) {
+		var view = new ViewClass(configuration || {});
+		view.render();
+		$(this.mainContainer).html(view.el);
+	},
 
-  poisIndex: function() {
-    var poiCollectionView = new PoiCollectionView({
-      collection: poiCollection
-    });
-    poiCollectionView.render();
-    $('#container').html(poiCollectionView.el);
-    console.log("poisIndex route");
-  },
-  poisShow: function(id) {
-    var model = poiCollection.find(function (poi) {
-      return poi.id === +id;
-    });
-    var poiModelView = new PoiModelView({
-      model: model
-    });
-    poiModelView.render();
-    $('#container').html(poiModelView.el);
-    console.log("poisShow route " + id);
-  },
+	home: function() {
+		this.renderBaseRoute(HomeView);
+	},
+
+	about: function() {
+		this.renderBaseRoute(AboutView);
+	},
+
+	poisIndex: function() {
+		this.renderBaseRoute(PoiCollectionView, {
+			collection: poiCollection
+		});
+	},
+	poisShow: function(id) {
+		var model = poiCollection.find(function (poi) {
+			return poi.id === +id;
+		});
+		this.renderBaseRoute(PoiModelView, {
+			model: model
+		});
+	},
 });
 
